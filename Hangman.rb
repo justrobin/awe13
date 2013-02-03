cedde72e55b0208e9a600292ad3546acf28ed617
Robin <robinp@tzi.de>
2013-02-03 11:12:38 +0100

    Hangman nutzt jetzt alle Buchstaben als UPCASE

--- Hangman.rb
+++ Hangman.rb
@@ -32,99 +32,105 @@ class Hangman
 			end
       if !@@zuraten.include?("_ ")
 				@@erraten = true
-				print "Geloest! Gratulation.\n"
+				print "Geloest! Gratulation.\n Das Wort lautet:\n"
 			end
     else
-    	@@geraten.push(char)
-			if @@geraten.length == 1
-				print "         \n"
-				print "         \n"
-				print "         \n"
-				print "         \n"
-				print "         \n"
-				print " _____  \n"
-				print "\/     \\  \n"
-			elsif @@geraten.length == 2
-				print "         \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print " __|__   \n"
-				print "\/     \\  \n"
-			elsif @@geraten.length == 3
-				print "         \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 4
-				print "   ----- \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 5
-				print "   ----- \n"
-				print "   |/  | \n"
-				print "   |     \n"
-				print "   |     \n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 6
-				print "   ----- \n"
-				print "   |/  | \n"
-				print "   |   o \n"
-				print "   |     \n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 7
-				print "   ----- \n"
-				print "   |/  | \n"
-				print "   |   o \n"
-				print "   |   | \n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 8
-				print "   ----- \n"
-				print "   |/  | \n"
-				print "   |   o \n"
-				print "   |  -|-\n"
-				print "   |     \n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-			elsif @@geraten.length == 9
-				print "   ----- \n"
-				print "   |/  | \n"
-				print "   |   o \n"
-				print "   |  -|-\n"
-				print "   |  / \\\n"
-				print " _/|\\_   \n"
-				print "/     \\  \n"
-				print "Verloren!"
-				@@erraten = true
+			if(!@@geraten.include?(char))
+    		@@geraten.push(char)
+				if @@geraten.length == 1
+					print "         \n"
+					print "         \n"
+					print "         \n"
+					print "         \n"
+					print "         \n"
+					print " _____  \n"
+					print "\/     \\  \n"
+				elsif @@geraten.length == 2
+					print "         \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print " __|__   \n"
+					print "\/     \\  \n"
+				elsif @@geraten.length == 3
+					print "         \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 4
+					print "   ----- \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 5
+					print "   ----- \n"
+					print "   |/  | \n"
+					print "   |     \n"
+					print "   |     \n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 6
+					print "   ----- \n"
+					print "   |/  | \n"
+					print "   |   o \n"
+					print "   |     \n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 7
+					print "   ----- \n"
+					print "   |/  | \n"
+					print "   |   o \n"
+					print "   |   | \n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 8
+					print "   ----- \n"
+					print "   |/  | \n"
+					print "   |   o \n"
+					print "   |  -|-\n"
+					print "   |     \n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+				elsif @@geraten.length == 9
+					print "   ----- \n"
+					print "   |/  | \n"
+					print "   |   o \n"
+					print "   |  -|-\n"
+					print "   |  / \\\n"
+					print " _/|\\_   \n"
+					print "/     \\  \n"
+					print "Verloren!\n"
+					exit
+					@@erraten = true
+				end
+			elsif
+				print "Buchstabe bereits geraten.\n"
 			end
     end
   end
 
   print "Loesungswort: "
   input = gets
-  hangman = Hangman.new(input)
+	
+  hangman = Hangman.new(input.upcase)
 
   while !@@erraten
-    print "Buchstabe: "
+    print "\nBuchstabe: "
     char = gets
     if char.length == 2
-      hangman.neuesZeichen(char.chomp)
+      hangman.neuesZeichen((char.chomp).upcase)
     else
-      print "Immer nur einen Buchstaben eingeben!\n"
+      print "Immer genau einen Buchstaben eingeben!\n"
     end
     hangman.zeigeFortschritt()
   end
