944e7b9832f34500ec2ddfd1481a52a1b583589a
Robin <robinp@tzi.de>
2013-02-03 08:43:55 +0100

    Hangman um zusätzliche Features erweitert

--- Hangman.rb
+++ Hangman.rb
@@ -1,12 +1,131 @@
 class Hangman
-	attr_reader :wort
+  @@wort = []
+  @@geraten = []
+  @@zuraten = []
+  @@erraten = false
   def initialize(wort)
-    @wort = wort.scan( /\w+/ )
+    @@wort = wort.scan(/./)
+		i = 0
+    while i < wort.length-1
+      @@zuraten[i] = "_ "
+      i += 1
+    end
   end
-  def rateBuchstabe(char)
-    
-  end
-  def showProgress()
-    print wort
+
+  def zeigeFortschritt()
+    i = 0
+    while i < @@zuraten.length
+      print "#{@@zuraten[i]}"
+      i += 1
+    end
+    print "\ngeratene Buchstaben: #{@@geraten}\n"
 	end
+
+  def neuesZeichen(char)
+    if @@wort.include?(char)
+			i = 0
+			while i < @@wort.length
+				if @@wort[i] == char
+					@@zuraten[i] = char +" "
+				end
+				i += 1
+			end
+      if !@@zuraten.include?("_ ")
+				@@erraten = true
+				print "Geloest! Gratulation.\n"
+			end
+    else
+    	@@geraten.push(char)
+			if @@geraten.length == 1
+				print "         \n"
+				print "         \n"
+				print "         \n"
+				print "         \n"
+				print "         \n"
+				print " _____  \n"
+				print "\/     \\  \n"
+			elsif @@geraten.length == 2
+				print "         \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print " __|__   \n"
+				print "\/     \\  \n"
+			elsif @@geraten.length == 3
+				print "         \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 4
+				print "   ----- \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 5
+				print "   ----- \n"
+				print "   |/  | \n"
+				print "   |     \n"
+				print "   |     \n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 6
+				print "   ----- \n"
+				print "   |/  | \n"
+				print "   |   o \n"
+				print "   |     \n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 7
+				print "   ----- \n"
+				print "   |/  | \n"
+				print "   |   o \n"
+				print "   |   | \n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 8
+				print "   ----- \n"
+				print "   |/  | \n"
+				print "   |   o \n"
+				print "   |  -|-\n"
+				print "   |     \n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+			elsif @@geraten.length == 9
+				print "   ----- \n"
+				print "   |/  | \n"
+				print "   |   o \n"
+				print "   |  -|-\n"
+				print "   |  / \\\n"
+				print " _/|\\_   \n"
+				print "/     \\  \n"
+				print "Verloren!"
+				@@erraten = true
+			end
+    end
+  end
+
+  print "Loesungswort: "
+  input = gets
+  hangman = Hangman.new(input)
+
+  while !@@erraten
+    print "Buchstabe: "
+    char = gets
+    if char.length == 2
+      hangman.neuesZeichen(char.chomp)
+    else
+      print "Immer nur einen Buchstaben eingeben!\n"
+    end
+    hangman.zeigeFortschritt()
+  end
 end
